d46c0717fbdb26807749e55a644789bc6dacbe02
Oscar Villette <2321969V@student.gla.ac.uk>
2021-05-27 16:30:50 +0200

    Tested Containers, Padding and Rows

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Remind Me!'),
-      centerTitle: true,
-      backgroundColor: Colors.red,
-    ),
-    body: Center(
-      child: Text(
-        'Hello World',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[600],
-          fontFamily: 'IndieFlower',
-        ),
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Remind Me!'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('New'),
-      onPressed: () {},
-      backgroundColor: Colors.red,
-    ),
-  ),
-));
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('Hello World'),
+          TextButton(
+            onPressed: () {}, 
+            child: Text('Click me'),
+            style: TextButton.styleFrom(
+              primary: Colors.red,
+              backgroundColor: Colors.amber,
+            ),
+          ),
+          Container(
+            color: Colors.cyan,
+            padding: EdgeInsets.all(30.0),
+            child: Text('Inside container'),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('New'),
+        onPressed: () {},
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
